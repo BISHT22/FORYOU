186e6beb2f74dff66939fb0d337f239c23803530
BISHT22 <163825508+BISHT22@users.noreply.github.com>
2024-03-20 16:22:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 
-function moveRandomEl(elm) {
-    elm.style.position = "absolute";
-    elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
-    elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
-  }
-  
-  const moveRandom = document.querySelector("#move-random");
-  
-  moveRandom.addEventListener("mouseenter", function (e) {
-    moveRandomEl(e.target);
-  });
+const coinIcon = document.getElementById('coin'); 
+const tossBtn = 
+	document.getElementById('toss-button'); 
+const result = 
+	document.querySelector('.result'); 
+coinIcon.insertAdjacentElement('afterend', result); 
+tossBtn.addEventListener('click', () => { 
+	tossBtn.disabled = true; 
+	tossCoinFunction(); 
+}); 
+function tossCoinFunction() { 
+	const randomVal = Math.random(); 
+	const faceCoin = randomVal < 0.5 ? 'Heads' : 'Tails'; 
+	const imageUrl = faceCoin === 'Heads' ? 
+'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpngtree.com%2Fso%2FHead-Coin&psig=AOvVaw24tM-UI9sm-eRfdZUiOI-r&ust=1711017767453000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJDN1oTUgoUDFQAAAAAdAAAAABAJ' : 
+'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngaaa.com%2Fdetail%2F894139&psig=AOvVaw06WV7QXHkKqONlaBOooyKJ&ust=1711017978607000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKiNiunUgoUDFQAAAAAdAAAAABAJ'; 
+		
+	coinIcon.classList.add('flip'); 
+	setTimeout(() => { 
+		coinIcon.innerHTML = 
+			`<img src="${imageUrl}" alt="${faceCoin}">`; 
+		coinIcon.classList.remove('flip'); 
+		setTimeout(() => { 
+			result.textContent = `Result: ${faceCoin}`; 
+			result.style.opacity = 1; 
+			tossBtn.disabled = false; 
+		}, 500); 
+	}, 1000); 
+}
+
